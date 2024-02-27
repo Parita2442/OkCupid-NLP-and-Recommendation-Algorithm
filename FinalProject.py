84162e9be32d0b1a6ff607a5caae9c7804f62016
Gunjan Sharma <88032850+S-Gunjan@users.noreply.github.com>
2024-02-26 21:49:12 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,1 +1,13 @@
+# Importing libraries and mounting the drive
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from google.colab import drive
+import seaborn as sns
+import statsmodels.formula.api as smf
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+!pip install -U googlemaps
+drive.mount('/content/drive')
+data_folder = '/content/drive/MyDrive/Colab Notebooks/BA820/Data/'
